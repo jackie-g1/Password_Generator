cdcd4bc78fc3b410fb7e4dfe65fdf65ccdb75f7a
jackie-g1 <104854186+jackie-g1@users.noreply.github.com>
2022-05-12 14:29:39 -0400

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,16 +1,16 @@
 import random
 
-num_of_pass = int(input("Hello. Welcome to the random password generator. How many passwords would you like? "))
-if num_of_pass > 0:
-    len_of_pass = int(input("Great. How long would you like your password? "))
-else:
-    print("Got it. Goodbye.")
-
 char = """"1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'\()*+,-./:;<=>?@"""
+num_of_pass = int(input("Hello. Welcome to the random password generator. How many passwords would you like? "))
+len_of_pass = int(input("Great. How long would you like your password? "))
 
-print('\nHere are your passwords: ')
-for pwd in range(num_of_pass):
-    passwords = ''
-    for c in range(len_of_pass):
-        passwords += random.choice(char)
-    print(passwords)
+if num_of_pass <= 0 or len_of_pass <= 0:
+    print("Unable to produce a password with current inputs. Goodbye.")
+else:
+    print('\nHere are your passwords: ')
+    for i in range(num_of_pass):
+        passwords = ''
+        for j in range(len_of_pass):
+            passwords += random.choice(char)
+        print(passwords)
+
